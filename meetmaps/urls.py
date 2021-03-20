5716a7a88ce20e33faf46f1d42d0195539c39308
Kavar814 <kavin.gmk@gmail.com>
2021-03-20 16:48:41 -0400

    Created main html templates

--- meetmaps/urls.py
+++ meetmaps/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls import url                                                                                                                              
+from django.conf.urls import url
+from django.urls import path                                                                                                                              
 from . import views
 
 urlpatterns = [ 
-    url(r'', views.default_map, name="default"),
+    path('', views.home_page, name='home'),
+    path('map/', views.map, name='map'),
+    path('events/', views.events, name='events'),
 ]
